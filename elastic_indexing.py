dea0219a132300bb602e22942d5546933885ba0c
mina-rf <rafiei.mina73@gmail.com>
2017-01-27 23:52:58 +0330

    Add delete index to elasticsearch

--- elastic_indexing.py
+++ elastic_indexing.py
@@ -9,7 +9,6 @@ from elasticsearch import Elasticsearch
 def make_index(dir):
     es = Elasticsearch()
     es.indices.create(index='wiki-index', ignore=400)
-    cwd = os.getcwd()
     BASE_DIR = os.path.dirname(os.path.realpath(__file__))
     dir = os.path.join(BASE_DIR, dir)
     all_files = [f for f in listdir(dir) if isfile(join(dir, f))]
@@ -19,4 +18,14 @@ def make_index(dir):
             es.index(index="wiki-index", doc_type='doc', body=body)
 
 
+def delete_index(index):
+    es = Elasticsearch()
+    es.indices.delete(index=index, ignore=[400, 404])
+
+
 make_index('files')
+es = Elasticsearch()
+res = es.search(index="wiki-index", body={"query": {"match": {'title':'سعدی'}}})
+print("Got %d Hits:" % res['hits']['total'])
+for hit in res['hits']['hits']:
+    print("%(title)s %(title)s: %(page)s" % hit["_source"])
