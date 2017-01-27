a2c447167ba8facc304251c1b7966edb44c68eaf
Melika Behjati <melikabehjati@gmail.com>
2017-01-27 13:20:26 +0330

    Create json file for each item

--- MIR_project3/pipelines.py
+++ MIR_project3/pipelines.py
@@ -4,8 +4,18 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+import os
 
 
 class MirProject3Pipeline(object):
     def process_item(self, item, spider):
+        file_name = item.get('title') + '.json'
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        p = os.path.join(BASE_DIR, 'files')
+        a = os.path.join(p, file_name)
+        self.file = open(a, 'w')
+        json.dump(dict(item), self.file)
+        self.file.close()
+
         return item
