c5258263cb19767c68e98c1670f6ad7df69bf469
Melika Behjati <melikabehjati@gmail.com>
2017-01-28 15:24:45 +0330

    Check if the file is hidden and Remove prev index

--- elastic_indexing.py
+++ elastic_indexing.py
@@ -1,22 +1,27 @@
+import json
 import os
 from os import listdir
 from os.path import isfile, join
 
-import json
+import elasticsearch
 from elasticsearch import Elasticsearch
 
 
 def make_index(dir):
     es = Elasticsearch()
+    try:
+        es.indices.delete(index='wiki-index', ignore=[400, 404])
+    except elasticsearch.NotFoundError:
+        pass
     es.indices.create(index='wiki-index', ignore=400)
-    cwd = os.getcwd()
     BASE_DIR = os.path.dirname(os.path.realpath(__file__))
     dir = os.path.join(BASE_DIR, dir)
-    all_files = [f for f in listdir(dir) if isfile(join(dir, f))]
+    all_files = [join(dir, f) for f in listdir(dir) if isfile(join(dir, f)) and not f.startswith('.')]
     for file in all_files:
-        with open(str(dir)+'/'+str(file)) as data_file:
+        with open(file) as data_file:
             body = json.load(data_file)
             es.index(index="wiki-index", doc_type='doc', body=body)
+    print('done')
 
 
 make_index('files')
